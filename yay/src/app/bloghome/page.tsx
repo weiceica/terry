c598f7a7a6dcf43a3c0e413d015ae1ecdf95aa51
Terry Shi <weiceica@gmail.com>
2023-10-26 11:11:29 -0400

    img test

---
+++ yay/src/app/bloghome/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function bloghome() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-20">
+        <h2>Bloghome</h2>
+    </main>
+  )
+}
+
+export default bloghome
--- yay/src/app/page.tsx
+++ yay/src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import './globals.css'
+import Link from 'next/link';
+import Image from 'next/image';
 import React, { useState, useEffect } from 'react';
 
+
 export default function Home() {
-  const [greeting, setGreeting] = useState("Welcome to");
+  const [greeting, setGreeting] = useState("Welcome!");
   const [fadeIn, setFadeIn] = useState(false);
 
   useEffect(() => {
@@ -23,18 +26,22 @@ export default function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-      <h1 className={`text-8xl font-['Exo_2'] font-bold text-gray-300 ${fadeIn ? 'floating-fade-in' : 'floating-fade-out'}`}>
-        {greeting}
-      </h1>
+        <div className="relative flex flex-col items-center justify-center">
+          <Image
+            src="/sideshot.jpg" // Updated the path to be relative to the 'public' directory
+            alt="Picture of Terry"
+            width={400} // Set the width as per your requirement
+            height={400} // Set the height as per your requirement
+            className="rounded-full"
+          />
+          <h1 className={`mt-4 text-8xl font-['Exo_2'] font-bold text-gray-900 dark:text-gray-300 ${fadeIn ? 'floating-fade-in' : 'floating-fade-out'}`}>
+            {greeting}
+          </h1>
+        </div>
     </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Link href="/bloghome" className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Blog{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -44,14 +51,9 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Learning about myself through documenting my life
           </p>
-        </a>
+        </Link>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Link href="/bloghome" className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Projects{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -61,7 +63,8 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Some cool projects I have been working on
           </p>
-        </a>
+        </Link>
+
 
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
