b378dfcc97b932d930e02fba50619f21a20b7608
Terry Shi <weiceica@gmail.com>
2023-11-04 23:09:01 -0400

    bloghome done

--- yay/src/app/bloghome/page.tsx
+++ yay/src/app/bloghome/page.tsx
@@ -1,16 +1,55 @@
-'use client'
-
-import React from 'react'
-import Image from 'next/image'
-import '../globals.css';
+import React from 'react';
 import Navbar from '../components/navbar';
+import Footer from '../components/footer';
+import Image from 'next/image';
+import imgBackShot from '../../../public/images/terryBack.png'
 
-function bloghome() {
+function BlogHome() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pb-20 pl-20 pr-20">
-        <Navbar />
-    </main>
-  )
+    <>
+      <Navbar/>
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 my-12 pt-36">
+        <article>
+          <header className="mb-10 text-center">
+            <h1 className="text-5xl font-bold mb-4">Terry's Blog</h1>
+            <div className="mx-auto my-2 h-1 w-16 bg-black mt-12 mb-12"></div>
+            <p className="text-sm text-gray-500">A Journey of self reflection</p>
+          </header>
+
+          <div className="mb-10 max-w-md mx-auto">
+            <p className="text-base text-gray-700 text-justify hover:scale-110">
+              Growing up, I was privileged enough to live in a variety of diverse places. I experienced the luxuries of Vancouver, spoiled with a blend of natural scenery and calm city life. I also experienced Beijing's rapidy changing, fast paced urban life. 
+            </p>
+            <br />
+            <p className="text-base text-gray-700 text-justify hover:scale-110 hover:z[-1]">
+              As a kid growing up, I didn't realize how lucky I am to get to travl to the edges of the world. I didn't realize how greatful I should be to have such loving parents. I didn't realize the starting point I had, and how greatful I should be.
+            </p>
+            <br />
+            <p className="text-base text-gray-700 text-center hover:scale-110 hover:z[-1]">
+              <b>scroll down for my Blogs!</b>
+            </p>
+          </div>
+
+
+          <figure className="mb-10 mt-10">
+            <Image
+              src={imgBackShot}
+              alt="Whatever"
+              width={900}
+              height={1600}
+              layout="responsive"
+              objectFit="contain"
+              quality={100}
+              className="rounded-2xl"
+            />
+          </figure>
+
+          
+        </article>
+      </main>
+      <Footer/>
+    </>
+  );
 }
 
-export default bloghome
+export default BlogHome;
--- yay/src/app/components/footer.tsx
+++ yay/src/app/components/footer.tsx
@@ -7,15 +7,15 @@ function Footer() {
     <footer className="fixed bottom-0 w-full bg-white shadow-md p-4 flex justify-center items-center">
       <div className="flex items-center space-x-8">
         {/* Email Icon */}
-        <Link href="mailto:your-email@weiceica@gmail.com" className="text-blue-600 hover:underline">
+        <Link href="mailto:your-email@weiceica@gmail.com" className="text-black hover:underline">
           <FaEnvelope size={25} />
         </Link>
         {/* GitHub Icon */}
-        <Link href="https://github.com/weiceica" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
+        <Link href="https://github.com/weiceica" target="_blank" rel="noopener noreferrer" className="text-black hover:underline">
           <FaGithub size={25} />
         </Link>
         {/* LinkedIn Icon */}
-        <Link href="https://www.linkedin.com/in/terry-shi-54a853128/" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
+        <Link href="https://www.linkedin.com/in/terry-shi-54a853128/" target="_blank" rel="noopener noreferrer" className="text-black hover:underline">
           <FaLinkedin size={25} />
         </Link>
       </div>
